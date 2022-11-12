db13aba593db3fc8486b3ff6bacfdfb9cd8da9ce
hyparh <hyparh@gmail.com>
2022-11-12 11:08:45 +0200

    Create buttons to iterate through each image

--- slider.js
+++ slider.js
@@ -2,9 +2,34 @@ const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const slider = document.querySelector(".slider");
 const images = document.querySelectorAll(".image");
+const bottom = document.querySelector(".bottom");
 
 let slideNumber = 1;
 const length = images.length;
+
+for (let i = 0; i < length; i++) {
+    const div = document.createElement("div");
+    div.className = "button";
+    bottom.appendChild(div);  
+};
+
+const buttons = document.querySelectorAll(".button");
+buttons[0].style.backgroundColor = "white";
+
+const resetBackground = () => {
+    buttons.forEach(button => {
+        button.style.backgroundColor = "transparent";
+    });
+};
+
+buttons.forEach((button, i) => {
+    button.addEventListener("click", () => {
+        resetBackground();
+        slider.style.transform = `translateX(-${i * 800}px)`;
+        slideNumber = i + 1;
+        button.style.backgroundColor = "white";
+    })
+});
 
 const nextSlide = () => {
     slider.style.transform = `translateX(-${slideNumber * 800}px)`;
@@ -27,9 +52,9 @@ const getLastSlide = () => {
 };
 
 right.addEventListener("click", () => {
-    slideNumber < length ? nextSlide() : getFirstSlide();
+    slideNumber < length ? nextSlide() : prevSlide();
 });
 
 left.addEventListener("click", () => {
-    slideNumber > 1 ? prevSlide() : nextSlide();
+    slideNumber > 1 ? prevSlide() : getLastSlide();
 });
